deb1b80f511e755acb0a9ce2d73c12e1a0e4f3d7
Mike Nelson <mike@mikeonrails.com>
2014-04-15 13:20:17 -0700

    fix dummy app

--- spec/internal/config/environments/test.rb
+++ spec/internal/config/environments/test.rb
@@ -7,7 +7,7 @@ Internal::Application.configure do
   # and recreated between test runs. Don't rely on the data there!
   config.cache_classes = true
   config.eager_load = true
-  
+
   # Configure static asset server for tests with Cache-Control for performance
   config.serve_static_assets = true
   config.static_cache_control = "public, max-age=3600"
@@ -31,7 +31,7 @@ Internal::Application.configure do
   config.action_mailer.delivery_method = :test
 
   # Raise exception on mass assignment protection for Active Record models
-  config.active_record.mass_assignment_sanitizer = :strict
+  # config.active_record.mass_assignment_sanitizer = :strict
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
